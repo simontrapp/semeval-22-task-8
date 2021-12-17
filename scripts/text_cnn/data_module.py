51c221c5d1429b0c354aef717cca209102296f1b
s364758 <felix.heinickel@stud-mail.uni-wuerzburg.de>
2021-12-17 23:02:45 +0100

    memory optimization

--- scripts/text_cnn/data_module.py
+++ scripts/text_cnn/data_module.py
@@ -1,7 +1,8 @@
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from preprocess import load_data, preprocess_data
+from preprocess import  preprocess_data
 from data_set import SentenceDataset
+from sentence_transformers import SentenceTransformer
 
 
 class DataModule(pl.LightningDataModule):
@@ -9,23 +10,32 @@ class DataModule(pl.LightningDataModule):
     val_ds = None
     test_ds = None
 
-    def __init__(self, embeddings_path: str, batch_size: int = 32):
+    def __init__(self, data_path, CSV_PATH, base_path, evaluation_ratio=0.2, test_ratio=0.2, batch_size: int = 32):
         super(DataModule, self).__init__()
         self.batch_size = batch_size
-        self.embeddings_path = embeddings_path
+
+        self.CSV_PATH = CSV_PATH
+        self.data_path = data_path
+        self.base_path = base_path
+        self.evaluation_ratio = evaluation_ratio
+        self.test_ratio = test_ratio
 
     def setup(self, stage):
-        training_sentences, training_scores, training_ids, \
-        validation_sentences, validation_scores, validation_ids, \
-        test_sentences, test_scores_normalized, test_scores_raw, test_ids = load_data(
-            self.embeddings_path)
+        bert = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
+        bert.max_seq_length = 512
 
-        print(f"Finished reading the data!\n# training sentence pairs: {len(training_sentences)}\n"
-              f"# evaluation sentence pairs: {len(validation_sentences)}\n"
-              f"# test sentence pairs: {len(test_sentences)}")
-        self.train_ds = SentenceDataset(training_sentences, training_scores)
-        self.val_ds = SentenceDataset(validation_sentences, validation_scores)
-        self.test_ds = SentenceDataset(test_sentences, test_scores_normalized)
+        training_sentences_1, training_sentences_2, training_scores, training_ids, \
+        evaluation_sentences_1, evaluation_sentences_2, evaluation_scores, evaluation_ids, \
+        test_sentences_1, test_sentences_2, test_scores_normalized, test_scores_raw, test_ids \
+            = preprocess_data(self.data_path, self.CSV_PATH, self.base_path, bert, create_test_set=True,
+                              validation_ratio=self.evaluation_ratio, test_ratio=self.test_ratio)
+
+        print(f"Finished reading the data!\n# training sentence pairs: {len(training_sentences_1)}\n"
+              f"# evaluation sentence pairs: {len(evaluation_sentences_1)}\n"
+              f"# test sentence pairs: {len(test_sentences_1)}")
+        self.train_ds = SentenceDataset(training_sentences_1,training_sentences_2, training_scores, bert)
+        self.val_ds = SentenceDataset(evaluation_sentences_1, evaluation_sentences_2, evaluation_scores, bert)
+        self.test_ds = SentenceDataset(test_sentences_1, test_sentences_2, test_scores_normalized, bert)
 
     def train_dataloader(self):
         return DataLoader(self.train_ds, shuffle=True, batch_size=self.batch_size)
--- scripts/text_cnn/data_set.py
+++ scripts/text_cnn/data_set.py
@@ -1,20 +1,29 @@
 from torch.utils.data import Dataset
 import torch
 from util import pad_input
+import numpy as np
 
 
 class SentenceDataset(Dataset):
 
-    def __init__(self, sentences, label):
+    def __init__(self, sentences_1, sentences_2, label, encoder):
         super(SentenceDataset, self).__init__()
-        self.sentences = sentences
+        max_1, max_2 = np.max([len(s) for s in sentences_1]), np.max([len(s) for s in sentences_2])
+        self.max = np.max(max_1,max_2)
+        self.sentences_1 = sentences_1
+        self.sentences_2 = sentences_2
         self.labels = label
-        self.len = len(sentences)
+        self.encoder = encoder
+        self.len = len(sentences_1)
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, idx):
-        s = self.sentences[idx]
+        e1 = self.encoder(self.sentences_1[idx])
+        e1 = np.concatenate((e1, np.zeros((max - e1.shape[0], e1.shape[1]))))
+        e2 = self.encoder(self.sentences_2[idx])
+        e2 = np.concatenate((e2, np.zeros((max - e2.shape[0], e2.shape[1]))))
+        embedding = np.array([e1,e2])
         label = self.labels[idx]
-        return torch.Tensor(s), torch.Tensor(label).float()
+        return torch.Tensor(embedding), torch.Tensor(label).float()
