44f53cd147b9b4ec47cf5e3c47717ddd76450546
s364758 <felix.heinickel@stud-mail.uni-wuerzburg.de>
2021-12-17 23:15:12 +0100

    bug fixes

--- scripts/text_cnn/data_set.py
+++ scripts/text_cnn/data_set.py
@@ -9,7 +9,7 @@ class SentenceDataset(Dataset):
     def __init__(self, sentences_1, sentences_2, label, encoder):
         super(SentenceDataset, self).__init__()
         max_1, max_2 = np.max([len(s) for s in sentences_1]), np.max([len(s) for s in sentences_2])
-        self.max = np.max(max_1,max_2)
+        self.max = np.max([max_1,max_2])
         self.sentences_1 = sentences_1
         self.sentences_2 = sentences_2
         self.labels = label
@@ -20,10 +20,10 @@ class SentenceDataset(Dataset):
         return self.len
 
     def __getitem__(self, idx):
-        e1 = self.encoder(self.sentences_1[idx])
-        e1 = np.concatenate((e1, np.zeros((max - e1.shape[0], e1.shape[1]))))
-        e2 = self.encoder(self.sentences_2[idx])
-        e2 = np.concatenate((e2, np.zeros((max - e2.shape[0], e2.shape[1]))))
+        e1 = self.encoder.encode(self.sentences_1[idx])
+        e1 = np.concatenate((e1, np.zeros((self.max - e1.shape[0], e1.shape[1]))))
+        e2 = self.encoder.encode(self.sentences_2[idx])
+        e2 = np.concatenate((e2, np.zeros((self.max - e2.shape[0], e2.shape[1]))))
         embedding = np.array([e1,e2])
         label = self.labels[idx]
         return torch.Tensor(embedding), torch.Tensor(label).float()
