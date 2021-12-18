818d36fd0e6a7ce90f9f23f31be77ead1216eee5
s364758 <felix.heinickel@stud-mail.uni-wuerzburg.de>
2021-12-18 16:02:59 +0100

    rolled back from pytorch-lightning

---
+++ scripts/text_cnn/text_cnn.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+from util import MaxOverTimePooling
+
+
+class TextCnn(nn.Module):
+    def __init__(self, loss_fn, device="cuda"):
+        super(TextCnn, self).__init__()
+
+        self.loss_fn = loss_fn
+        self.network_1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=50, kernel_size=3),
+            nn.Dropout(0.2),
+            nn.ReLU6(),
+            nn.MaxPool2d(kernel_size=(2, 3)),
+            nn.Conv2d(in_channels=50, out_channels=100, kernel_size=3),
+            nn.ReLU6(),
+            nn.MaxPool2d(kernel_size=(2, 3)),
+            nn.Conv2d(in_channels=100, out_channels=200, kernel_size=3),
+            nn.Dropout(0.2),
+            nn.ReLU6(),
+            MaxOverTimePooling()
+        )
+        self.network_2 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=50, kernel_size=3),
+            nn.Dropout(0.2),
+            nn.ReLU6(),
+            nn.MaxPool2d(kernel_size=(2, 3)),
+            nn.Conv2d(in_channels=50, out_channels=100, kernel_size=3),
+            nn.ReLU6(),
+            nn.MaxPool2d(kernel_size=(2, 3)),
+            nn.Conv2d(in_channels=100, out_channels=200, kernel_size=3),
+            nn.Dropout(0.2),
+            nn.ReLU6(),
+            MaxOverTimePooling()
+        )
+
+        self.final_network = nn.Sequential(
+            nn.Linear(in_features=400, out_features=100, bias=False),
+            nn.Dropout(0.2),
+            nn.ReLU6(),
+            nn.Linear(in_features=100, out_features=4, bias=False),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x_1 = x[:, 0, :, :]
+        x_1 = self.network_1(x_1.reshape(x_1.size()[0], 1, *x_1.size()[-2:]))
+        x_2 = x[:, 1, :, :]
+        x_2 = self.network_2(x_2.reshape(x_2.size()[0], 1, *x_2.size()[-2:]))
+        x = torch.cat((x_1, x_2), dim=1).reshape(x_1.size()[0], 400)
+        return self.final_network(x)
---
+++ scripts/text_cnn/train.py
@@ -0,0 +1,61 @@
+import math
+import torch
+from sklearn import metrics
+import pandas
+import time
+from tqdm import tqdm
+from torchmetrics.functional import accuracy, iou
+
+
+def _shared_eval_step(self, batch, batch_idx):
+    x, y = batch
+    y_hat = self(x)
+    loss = self.loss_fn(y_hat, y)
+    y = y.int()
+    acc = accuracy(y_hat, y)
+    _iou = iou(y_hat, y, num_classes=2)
+
+    return y_hat, loss, acc, _iou
+
+def train(model, loss_fn, optimizer, device, train_dataloader, result_path=None):
+    size = len(train_dataloader.dataset)
+    num_batches = math.ceil(size / train_dataloader.batch_size)
+    model.train()
+    loss_sum = 0
+    for batch_index, (X, y) in enumerate(tqdm(train_dataloader)):
+        X, y = X.to(device), y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        loss_sum += loss
+        #if batch_index % 1 == 0 or batch_index + 1 == num_batches:
+            #print(f"loss: {loss_sum / (batch_index + 1):>7f}  [{batch_index + 1:>5d}/{num_batches:>5d}]")
+
+
+def validate(model, device, dataloader, result_path=None, save_predictions=False, ids=None):
+    print("validate")
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = math.ceil(size / dataloader.batch_size)
+    p = None
+    l = None
+    for batch_index, (X, y) in enumerate(tqdm(dataloader)):
+        X, y = X.to(device), y.to(device)
+        pred = model(X)
+        if p is None:
+            p = pred
+            l = y
+        else:
+            p = torch.cat((p, pred), dim=0)
+            l = torch.cat((l, y), dim=0)
+
+    p = torch.argmax(p, dim=1).detach()
+    l = torch.argmax(l, dim=1).detach()
+    #print(metrics.classification_report(l, p))
+    acc = accuracy(p, l)
+    # log.info(f"accuracy: {accuracy}")
+
+    if save_predictions:
+        pandas.DataFrame({'pair_id': ids, 'predictions': p.numpy().tolist()}).to_csv(result_path, index=False)
