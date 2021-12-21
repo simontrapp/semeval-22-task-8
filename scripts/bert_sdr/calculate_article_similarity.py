72afa143b3d7949e28530945af917d5c58fbbbcf
simontrapp <simon.trapp115@gmail.com>
2021-12-21 13:34:34 +0100

    fixed out of memory errors. added random forest prediction output to csv.

--- scripts/bert_sdr/calculate_article_similarity.py
+++ scripts/bert_sdr/calculate_article_similarity.py
@@ -17,7 +17,7 @@ OUTPUT_CSV_PATH = '../../models/sdr_sbert_document_similarities.csv'
 
 
 # process article title (first) + text to a list of sentences.
-def process_json_to_sentences(path: str):
+def process_json_to_sentences(path: str, filter_sentence_length: bool = False, min_sentence_len: int = 15, max_sentence_len: int = 1500):
     with open(path, 'r') as file:
         article_data = json.load(file)
         title = article_data['title']
@@ -27,6 +27,15 @@ def process_json_to_sentences(path: str):
             res.append(title)
         res.extend(text_sentences)
         del article_data, title
+        if filter_sentence_length:
+            tmp = []
+            for sentence in res:
+                if len(sentence) > min_sentence_len:
+                    if len(sentence) < max_sentence_len:
+                        tmp.append(sentence)
+                    else:
+                        tmp.extend([sentence[i:min(i+max_sentence_len, len(sentence))] for i in range(0, len(sentence), max_sentence_len)])
+            return tmp
         return res
 
 
@@ -34,8 +43,14 @@ def create_sbert_embeddings(model: sentence_transformers.SentenceTransformer, se
     return model.encode(sentences, batch_size=4)
 
 
-def create_universal_sentence_encoder_embeddings(model, input_sentences: list):
-    return model(input_sentences)
+def create_universal_sentence_encoder_embeddings(model, input_sentences: list, batch_size: int = 50):
+    if len(input_sentences) > batch_size:      # prevent memory error by limiting number of sentences
+        res = []
+        for i in range(0, len(input_sentences), batch_size):
+            res.extend(model(input_sentences[i:min(i+batch_size, len(input_sentences))]))
+        return res
+    else:
+        return model(input_sentences)
 
 
 def append_output_sample(output_data: dict, pair_id_1: int, pair_id_2: int, ov_score: float, ss_2_1: float, ss_1_2: float, us_2_1: float, us_1_2: float):
@@ -74,8 +89,8 @@ def compute_similarities(data_folder: str, data_csv: str, output_csv: str, sbert
             # only add pair to data if pair was actually downloaded
             if os.path.exists(first_json_path) and os.path.exists(second_json_path):
                 # read data
-                sentences_1 = process_json_to_sentences(first_json_path)
-                sentences_2 = process_json_to_sentences(second_json_path)
+                sentences_1 = process_json_to_sentences(first_json_path, True)
+                sentences_2 = process_json_to_sentences(second_json_path, True)
                 # score similarities
                 if len(sentences_1) > 0 and len(sentences_2) > 0:
                     # create embeddings
@@ -83,6 +98,10 @@ def compute_similarities(data_folder: str, data_csv: str, output_csv: str, sbert
                     sbert_embeddings_2 = create_sbert_embeddings(sbert_embedding_model, sentences_2)
                     use_embeddings_1 = create_universal_sentence_encoder_embeddings(use_embedding_model, sentences_1)
                     use_embeddings_2 = create_universal_sentence_encoder_embeddings(use_embedding_model, sentences_2)
+                    assert len(sentences_1) == len(sbert_embeddings_1)
+                    assert len(sentences_1) == len(use_embeddings_1)
+                    assert len(sentences_2) == len(sbert_embeddings_2)
+                    assert len(sentences_2) == len(use_embeddings_2)
                     # create scores
                     sbert_sim_2_to_1, sbert_sim_1_to_2 = util.embeddings_to_scores(sbert_embeddings_1, sbert_embeddings_2, similarity_type='cosine')
                     use_sim_2_to_1, use_sim_1_to_2 = util.embeddings_to_scores(use_embeddings_1, use_embeddings_2, similarity_type='arccosine')
@@ -92,6 +111,8 @@ def compute_similarities(data_folder: str, data_csv: str, output_csv: str, sbert
                     append_output_sample(output_data, pair_id_1, pair_id_2, overall_score, sbert_sim_2_to_1, sbert_sim_1_to_2, use_sim_2_to_1, use_sim_1_to_2)
                     print(f"Processed {index}: #sentences_1: {len(sentences_1)}, #sentences_2: {len(sentences_2)}")
                     del sentences_1, sentences_2, sbert_embeddings_1, sbert_embeddings_2, use_embeddings_1, use_embeddings_2
+    except AssertionError as e:
+        print(f"Error: {e}")
     except:
         print("Error occurred! Saving results...")
     finally:
