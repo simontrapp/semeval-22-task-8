59e1ba6d2d3b28ea39a579111b791bd610980fa4
s364758 <felix.heinickel@stud-mail.uni-wuerzburg.de>
2021-12-22 15:03:27 +0100

    added cnn after lstm

--- scripts/text_cnn/lstm.py
+++ scripts/text_cnn/lstm.py
@@ -12,10 +12,10 @@ class lstm_model(nn.Module):
         self.network_2 = InputNetwork(200, 2, embedding_length=self.embedding_length)
 
         self.final_network = nn.Sequential(
-            nn.Linear(in_features=800, out_features=200),
-            nn.Dropout(0.2),
-            nn.ReLU6(),
-            nn.BatchNorm1d(200),
+            #nn.Linear(in_features=800, out_features=200),
+            #nn.Dropout(0.2),
+            #nn.ReLU6(),
+            #nn.BatchNorm1d(200),
             nn.Linear(in_features=200, out_features=100),
             nn.ReLU6(),
             nn.BatchNorm1d(100),
@@ -39,9 +39,19 @@ class InputNetwork(nn.Module):
     def __init__(self, hidden_size, num_layers, embedding_length=768):
         super(InputNetwork, self).__init__()
 
-        self.lstm = nn.LSTM(input_size=embedding_length, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=embedding_length, hidden_size=hidden_size, num_layers=num_layers,
+                            bidirectional=True)
+        self.network = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=(5, 10), stride=(2, 4)),
+            nn.ReLU6(),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 100, kernel_size=(5, 10), stride=(2, 4)),
+            nn.ReLU6(),
+            nn.BatchNorm2d(100),
+            MaxOverTimePooling()
+        )
 
     def forward(self, x):
         output, (hn, cn) = self.lstm(x)
 
-        return output[:,-1,:]
+        return self.network(output.resize(output.size()[0], 1, *output.size()[-2:]))
